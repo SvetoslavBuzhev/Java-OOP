8ae14ee26e66a78fe35f6968592b40d29a1f9603
Svetoslav Buzhev <89996176+SvetoslavBuzhev@users.noreply.github.com>
2022-06-30 16:23:17 +0300

    Add files via upload

---
+++ Encapsulation/src/ValidationData/Main.java
@@ -0,0 +1,35 @@
+package ValidationData;
+
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        List<Person> people = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split("\\s+");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+            try {
+                Person person = new Person(firstName, lastName, age, salary);
+                people.add(person);
+            }catch (Exception e){
+                    System.out.println(e.getMessage());
+            }
+        }
+        double bonus = Double.parseDouble(scan.nextLine());
+
+        for (Person person:people) {
+            person.increaseSalary(bonus);
+            System.out.println(person);
+        }
+    }
+}
