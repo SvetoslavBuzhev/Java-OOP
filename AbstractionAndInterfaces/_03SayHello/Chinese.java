a42e7600e4b04a49a3f38a2ea6f03e2ff59c87e0
Svetoslav Buzhev <89996176+SvetoslavBuzhev@users.noreply.github.com>
2022-07-08 10:31:50 +0300

    Add files via upload

---
+++ AbstractionAndInterfaces/_03SayHello/Chinese.java
@@ -0,0 +1,17 @@
+package _03SayHello;
+
+public class Chinese implements Person{
+    private String name;
+    public Chinese(String name){
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
